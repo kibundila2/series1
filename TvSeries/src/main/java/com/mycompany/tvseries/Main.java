5710d2c1b17d37cc4745c95efbc88d85ededa4c4
IsraelKamunga <19kk145@esisalama.org>
2025-09-03 14:51:11 +0200

    bug fix

--- TvSeries/src/main/java/com/mycompany/tvseries/Main.java
+++ TvSeries/src/main/java/com/mycompany/tvseries/Main.java
@@ -4,7 +4,6 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello word");
         Scanner scanner = new Scanner(System.in);
         SeriesCore  series = new SeriesCore(scanner);
         series.StartSeriesApplication();
